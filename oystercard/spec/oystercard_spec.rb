d0086ce0546e10cf563a9e06e110d8b584b7d2db
Matt Zimmer <zimmermja@gmail.com>
2021-10-25 15:41:12 +0100

    top-up implemented, passed test

--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -1,7 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it "has a balance on creation" do
     expect(subject).to respond_to(:balance)
   end
+
+  it "balance increases by amount in top-up method" do
+    check_top_up(20,20)
+    check_top_up(20,40)
+  end
+
+  def check_top_up(num, expect)
+    subject.top_up(num)
+    expect(subject.balance).to eq expect
+  end
+
 end
